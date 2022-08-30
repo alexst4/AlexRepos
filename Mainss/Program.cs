5eca5191bb7c13a5db68254913843f14ffdb825e
alexst4 <alexstrombom8@gmail.com>
2022-08-30 16:29:01 +0200

    Mainss

---
+++ Mainss/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int characterAge;
+characterAge = 43;
+
+
+string characterName = "Alexander";
+
+Console.WriteLine("There was once a king named " + characterName + " the great");
+Console.WriteLine("King " + characterName + " was " + characterAge + " years old");
+Console.WriteLine("He really liked his name");
+
+Console.WriteLine("But hated his age of " + characterAge +" years");
+
+
+Console.ReadLine();
